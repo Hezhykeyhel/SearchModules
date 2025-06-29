769e4a124bf8e3e4a413727e4e4e566e40687f21
Egbetayo Damilola <76609799+Hezhykeyhel@users.noreply.github.com>
2025-06-29 18:38:00 +0100

    feat: updated search

---
+++ screens/search.tsx
@@ -0,0 +1,12 @@
+import { Box, Text } from "@/components/Restyle";
+import React from "react";
+
+const Search = () => {
+  return (
+    <Box alignItems="center" justifyContent="center" flex={1}>
+      <Text variant="bold16">Search</Text>
+    </Box>
+  );
+};
+
+export default Search;
